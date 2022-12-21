9cc2caf7976b28c4aadcf1d5c390d55c71a38a91
santosh <shirkes618@gmail.com>
2022-12-21 19:00:36 +0530

    array 1st version

---
+++ Example/src/com/velocity/Sample.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+public class Sample 
+{
+public static void main(String[] args) 
+{
+	int [] x= {10,20,30,40,50};
+	
+	
+	for(int i:x)
+	{
+		System.out.println(x[i]);
+	}
+	
+}
+}
